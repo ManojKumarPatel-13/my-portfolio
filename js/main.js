6efe50b7c766ca58e85400e09b7a9674b624753b
MANOJ KUMAR PATEL <manojkpatel1308@gmail.com>
2026-07-16 16:16:02 +0530

    feat: setup the latest of blogs section

--- js/main.js
+++ js/main.js
@@ -3,9 +3,11 @@ import { initTimeline } from "./modules/timeline.js";
 import { initProjects } from "./modules/projects.js";
 import { initValidation } from "./modules/validation.js";
 import { initContactFaqAndFooter } from "./modules/contacts-faq.js";
+import { initBlogs } from "./modules/blogs.js";
 
 initTechOrbit()
 initTimeline()
 initProjects()
 initValidation()
+initBlogs
 initContactFaqAndFooter()
